ba5923b79e9a58a30fc5478cf1be01d942d03e23
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-09 20:37:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function depthFirstSearch(rootNode, vertices, edges){
+  const store = [];
+  store.push(rootNode);
+  const visited = [rootNode];
+
+  while(store.length > 0){
+    const vertice = store.pop();
+
+    if(!vertice.discovered){
+      vertice.discovered = true;
+      const adjacentVertices = findAdjacent(vertice.name, vertices, edges);
+
+      adjacentVertices.forEach(vert => {
+        visited.push(vert);
+        store.push(vert);
+      })
+    }
+  }
+  
+};
